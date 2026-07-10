e109674e493111b1b2049732bdd54ba878e2e0d5
Bryce Bond <brycecbond94@gmail.com>
2026-07-10 10:29:17 +0200

    fix: add favicon

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
